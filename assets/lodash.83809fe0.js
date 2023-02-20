895db7727fa98e996fadfbe26fe354c890d896d0
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2023-02-20 12:26:51 +0600

    updated

--- assets/lodash.83809fe0.js
+++ assets/lodash.83809fe0.js
@@ -1,4 +1,4 @@
-import{O as jt}from"./index.435b05e6.js";var Ri={exports:{}};/**
+import{O as jt}from"./index.a8157cdc.js";var Ri={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
